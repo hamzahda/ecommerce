e3eaeae97c2b1b3b19e239a9f0f986b80390bd22
Hamza <hamzahda@outlook.com>
2020-05-28 13:52:01 +0200

    dtos for the auth

---
+++ src/main/java/com/website/dto/TokenUserResponseDTO.java
@@ -0,0 +1,37 @@
+package com.website.dto;
+
+
+
+
+public class TokenUserResponseDTO {
+
+	private String token;
+
+	private boolean error;
+
+	private String message;
+
+	public String getToken() {
+		return token;
+	}
+
+	public void setToken(String token) {
+		this.token = token;
+	}
+
+	public boolean getError() {
+		return error;
+	}
+
+	public void setError(boolean error) {
+		this.error = error;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+}
